de414323f925ef3000958344682d9f5f41526290
muhanad abukar <muhanadabdullahiabukar@gmail.com>
2022-11-14 20:08:43 +0100

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static int len(int num)
+        {
+            int len = 0;
+            for (int i = 1; i < num; i *= 10)
+            {
+                len++;
+            }
+            return len;
+        }
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            len(num);
+        }
+        
+    }
+}
